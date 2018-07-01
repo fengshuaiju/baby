9596bc6b87db7ccb1f367dc2477e9916d8d5e088
fengshuaiju <shuaiju.feng@jjcargo.com>
2018-07-01 18:34:13 +0800

    -

--- src/main/java/com/feng/baby/adapter/controller/BannerController.java
+++ src/main/java/com/feng/baby/adapter/controller/BannerController.java
@@ -19,6 +19,7 @@ public class BannerController {
     //参数名	数据类型	备注	必填
     //type	String	Banner类型，后台自定义	X
     //key
+    @GetMapping("/list")
     public JSONObject list(@RequestParam String type,
                            @RequestParam String key){
         return JSONObject.parseObject("{\"code\":0,\"data\":[{\"businessId\":30164,\"dateAdd\":\"2018-03-12 17:48:47\",\"dateUpdate\":\"2018-05-19 11:24:33\",\"id\":5581,\"linkUrl\":\"\",\"paixu\":0,\"picUrl\":\"https://cdn.it120.cc/apifactory/2018/05/19/4e72a11f1b291f4cacd72262ca6e96f0.png\",\"remark\":\"丸子君原创作品\",\"status\":0,\"statusStr\":\"显示\",\"title\":\"清欢严选\",\"type\":\"start\",\"userId\":797}],\"msg\":\"success\"}");
